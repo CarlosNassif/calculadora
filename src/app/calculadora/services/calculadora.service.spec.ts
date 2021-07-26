bc474d55210a444df26977e2383b8432dc269c10
Carlos Nassif <costa.carlos12@gmail.com>
2021-07-26 07:37:30 -0300

    Adicionar testes das operacoes feitas pelo servico da calculadora

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -13,4 +13,29 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Deve garantir que 5 + 9 = 14', () => {
+    const soma = service.calcular(5, 9, CalculadoraService.SOMA);
+    expect(soma).toEqual(14);
+  })
+
+  it('Deve garantir que 5 - 9 = -4', () => {
+    const sub = service.calcular(5, 9, CalculadoraService.SUBTRACAO);
+    expect(sub).toEqual(-4);
+  })
+
+  it('Deve garantir que 5 / 2 = 2.5', () => {
+    const div = service.calcular(5, 2, CalculadoraService.DIVISAO);
+    expect(div).toEqual(2.5);
+  })
+
+  it('Deve garantir que 5 * 9 = 45', () => {
+    const mult = service.calcular(5, 9, CalculadoraService.MULTIPLICACAO);
+    expect(mult).toEqual(45);
+  })
+
+  it('Deve garantir que operações com operador não esperado retorne 0', () => {
+    const res = service.calcular(5, 9, '%');
+    expect(res).toEqual(0);
+  })
 });
